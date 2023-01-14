5d54c1f17633bda48c88ff1acd0256293049546a
sgr2002-xd <20pc14@psgtech.ac.in>
2023-01-14 15:22:30 +0530

    hello

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import truecallerjs from "truecallerjs";
 import bodyParser from "body-parser";
 var app = express(); 
 import path from "path"
+import fs  from "fs";
 const __dirname = path.resolve();
 app.get('/',function(req,res){ 
   res.sendFile(path.join(__dirname+'/index.html')); 
@@ -22,12 +23,31 @@ app.post('/info', function (req, res) {
     
     var sn = truecallerjs.searchNumber(searchData);
     sn.then(function(response) {
-        console.log(response)
+        //console.log(response)
         res.writeHead(200);
-        res.end(JSON.stringify(response, null, 3));
+        //res.end(JSON.stringify(response, null, 3));
+        fs.writeFile('users.json', JSON.stringify(response, null, 3), err => {
+            if (err) {
+              throw err
+            }
+            console.log('JSON data is saved.')
+          })
+    //     fs.readFile('users.json',
+    //       // callback function that is called when reading file is done
+    //       function(err, data) {       
+    //           // json data
+    //           var jsonData = data;
+       
+    //           // parse json
+    //           var jsonParsed = JSON.parse(jsonData);
+       
+    //           // access elements
+    //           res.end("Name is: "+jsonParsed.data[0].name);
+              
+    //   });
     }); 
-    
  });  
+
 app.listen(8080); 
 console.log("Server running at Port 8080");  
 
